68e52072afb51f388a173cf6fbeacdb4b7bb6754
diogeneshas <diogenes.henrique.br@gmail.com>
2023-04-27 09:30:20 -0300

    Testes para model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value("diogenes@gmail.com").for(:email) }
 end
