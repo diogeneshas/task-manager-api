60cbefffc84f6fa26d7e471d11ccdff2169fe8b0
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-12 07:56:20 -0300

    Criando o model task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_095849) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_105408) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "done", default: false
+    t.datetime "deadline"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_095849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
