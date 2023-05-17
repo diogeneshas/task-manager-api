160b2c2e0293f980c9c41adba9e09f29cf43930e
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-17 07:53:51 -0300

    Adicionando o Controller de Tarefas (Action Show)

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::TasksController < ApplicationController
     tasks = current_user.tasks
     render json: { tasks: tasks }, status: 200
   end
+
+  def show
+    tasks = current_user.tasks.find(params[:id])
+    render json: tasks, status: 200
+  end
 end
